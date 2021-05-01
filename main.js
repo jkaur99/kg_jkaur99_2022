0df7279f29eca2d950eec9f6a6971e415beff395
jkaur99 <45667094+jkaur99@users.noreply.github.com>
2021-04-30 21:57:31 -0400

    works correctly with double digit integer input

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-//console.log(process.argv);
-
-var myArgs = process.argv.slice(2);
-//var newArgs = myArgs.split(" ");
-//console.log('myArgs: ', myArgs);
-
-var alphaNums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+var alphaNums = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 
 var ans = [];
-var i;
 
-for (i = 0 ; i < myArgs.length; i++ ){
-    
-    ans.push(alphaNums[myArgs[i]]);
-    
+var inputArr = process.argv.slice(2);
+
+inputArr.join(" ");
+
+for(var i=0; i < inputArr.length; i++){
+    var num = inputArr[i]; //get number at current index in input
+    num = "" + num; //convert number to string
+    num = num.split(""); //split the string by each character
+    var numInWords = "";
+    for(var j=0; j < num.length;  j++){ // loop through digits in number at index
+        numInWords += alphaNums[parseInt(num[j])]; //parse string back into int and get corressponding word from alphaNums array
+    }
+    ans.push(numInWords); //add word into answer array
 }
 
 console.log(ans);
